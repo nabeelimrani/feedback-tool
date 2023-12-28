4e87c0fe732f3718d1debd9f5af24a17490ae6b6
Nabeel Mehdi Imrani <nabeelemraane09@gmail.com>
2023-12-29 03:13:37 +0500

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,18 +1,21 @@
-<div class="container">
-    <!-- Your existing content goes here -->
-
-    <!-- Footer Section -->
-    <footer class="mt-5">
-        <div class="text-center">
-            <p>&copy; 2023 Feedback Tool. All rights reserved.</p>
+<body class="d-flex flex-column min-vh-100">
+    <footer class="mt-auto bg-light py-3">
+        <div class="container text-center">
+            <strong>Copyright &copy; 2023-2024 <a href="{{ url('/home') }}">Feedback Tool</a>.</strong> All rights
+            reserved.
         </div>
     </footer>
-</div>
+    <script src="{{ asset('frontend/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('frontend/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('frontend/dist/js/adminlte.min.js') }}"></script>
+    <script src="{{ asset('fontawesome/js/fontawesome.min.js') }}"></script>
+    <script src="{{ asset('fontawesome/js/all.min.js') }}"></script>
+    <script src="{{ asset('frontend/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('frontend/plugins/jquery-validation/additional-methods.min.js') }}"></script>
+    <script src="{{ asset('frontend/dist/js/demo.js') }}"></script>
+    <script src="{{ asset('frontend/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
+    <script src="{{ asset('frontend/plugins/toastr/toastr.min.js') }}"></script>
 
-<!-- Bootstrap and Font Awesome Scripts -->
-<script src="{{ asset('js/bootstrap.bundle.js') }}"></script>
-<script src="{{ asset('fontawesome/js/fontawesome.min.js') }}"></script>
-<script src="{{ asset('fontawesome/js/all.min.js') }}"></script>
 </body>
 
 </html>
